9f81a18f8ed3dfd7e4823bb727ccc226f3654d87
Moyenne <suedoo62@gmail.com>
2021-04-30 12:16:23 -0700

    Added user registration.

--- lossbrigadellc_lastshop/settings.py
+++ lossbrigadellc_lastshop/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'lastshop.apps.LastshopConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
